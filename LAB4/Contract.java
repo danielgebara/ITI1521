e2fa6ceae1cf49fedfa48863466701df0dbea863
danielgebara <128635331+danielgebara@users.noreply.github.com>
2025-02-06 22:53:30 -0500

    LAB4 finished

--- LAB4/Contract.java
+++ LAB4/Contract.java
@@ -8,20 +8,30 @@ public class Contract extends Employee {
     private static final double dueHour  = 40.0;
   
     Contract(String name) {
-    //VOTRE CODE VIENT ICI
+        super(name); 
     }
   
     Contract (String name, double nbHours, double hourRate, double percentHourSup) {
-    //VOTRE CODE VIENT ICI
+        super(name);
+        setSalaryInfo(nbHours, hourRate, percentHourSup);
     }
   
     public void setSalaryInfo(double nbHours, double hourRate, double percentHourSup) {
-    //VOTRE CODE VIENT ICI
+        this.nbHours = nbHours;
+        this.hourRate = hourRate;
+        this.percentHourSup = percentHourSup;
     }
     
-     
     public double getSalary() {
-    //VOTRE CODE VIENT ICI
+        double salary = 0.0;
+        if (nbHours > 0 && hourRate > 0 && percentHourSup > 0) {
+            if (nbHours <= dueHour) {
+                salary = nbHours * hourRate;
+            } else {
+                salary = dueHour * hourRate + (nbHours - dueHour) * hourRate * (1 + percentHourSup / 100);
+            }
+        }
+        return salary;
     }
   
    public double getNbHours() {  
